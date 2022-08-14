12bdcb6d5abd4e7d5e9068e7ac882180d2c8e3f8
Reinaldo <naldos15@hotmail.com>
2022-08-14 00:24:39 -0300

    tratamento de excecao ao salvar tecnico

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,13 +10,34 @@ import { Router } from '@angular/router';
 })
 export class TecnicoCreateComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  tecnico: Tecnico = {
+    id: '',
+    nome: 'Valdir',
+    cpf: '168.208.570-80',
+    telefone: '(62) 99021-5625'
+  }
+
+  constructor(
+    private router: Router,
+    private service: TecnicoService) { }
 
   ngOnInit(): void {
   }
 
   cancel():void {
     this.router.navigate(['tecnicos'])
+  }
+  
+  //Na funcao lambda, vai ser imprimido o erro no console do navegador
+  create(): void{
+    this.service.create(this.tecnico).subscribe((resposta) => {
+      this.router.navigate(['tecnicos'])
+      this.service.message('Tecnico criado com sucesso!')
+    }, err =>{
+      if(err.error.error.match('já cadastrado')){
+        this.service.message(err.error.error)
+      }
+    })
   }
 
 }
