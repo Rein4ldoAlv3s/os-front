3cfb82fb2c094b9587762a7a7be428527fedd88d
Reinaldo <naldos15@hotmail.com>
2022-08-14 16:18:34 -0300

    tratamento de excessoes ao criar tecnico

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Tecnico } from 'src/app/models/tecnico';
 import { TecnicoService } from 'src/app/services/tecnico.service';
@@ -12,10 +13,14 @@ export class TecnicoCreateComponent implements OnInit {
 
   tecnico: Tecnico = {
     id: '',
-    nome: 'Valdir',
-    cpf: '168.208.570-80',
-    telefone: '(62) 99021-5625'
+    nome: '',
+    cpf: '',
+    telefone: ''
   }
+
+  nome = new FormControl('', [Validators.minLength(5)])
+  cpf = new FormControl('', [Validators.minLength(11)])
+  telefone = new FormControl('', [Validators.minLength(11)])
 
   constructor(
     private router: Router,
@@ -24,20 +29,44 @@ export class TecnicoCreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  cancel():void {
+  cancel(): void {
     this.router.navigate(['tecnicos'])
   }
-  
+
   //Na funcao lambda, vai ser imprimido o erro no console do navegador
-  create(): void{
+  create(): void {
     this.service.create(this.tecnico).subscribe((resposta) => {
       this.router.navigate(['tecnicos'])
       this.service.message('Tecnico criado com sucesso!')
-    }, err =>{
-      if(err.error.error.match('já cadastrado')){
+    }, err => {
+      if (err.error.error.match('já cadastrado')) {
         this.service.message(err.error.error)
+      } else if (err.error.errors[0].message == "número do registro de contribuinte individual brasileiro (CPF) inválido") {
+        this.service.message("CPF inválido!")
+        console.log(err)
       }
     })
+  }
+
+  errorValidName(){
+    if(this.nome.invalid){
+      return 'O nome deve ter entre 5 e 100 caracteres!';
+    }
+    return false;
+  }
+
+  errorValidCpf(){
+    if(this.cpf.invalid){
+      return 'O CPF deve ter entre 11 e 15 caracteres';
+    }
+    return false;
+  }
+
+  errorValidTelefone(){
+    if(this.telefone.invalid){
+      return 'O nome deve ter entre 1 e 18 caracteres!';
+    }
+    return false;
   }
 
 }
